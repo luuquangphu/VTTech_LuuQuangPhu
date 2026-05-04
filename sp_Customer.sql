6d51be6a86efac0d8e5f3de17826b7df62f2a76f
Lưu Quang Phú <luuquangphuii@gmail.com>
2026-05-04 10:55:45 +0700

    Thêm phân trang cho LoadList; LoadDetail, Update, Delete theo ID

--- sp_Customer.sql
+++ sp_Customer.sql
@@ -2,34 +2,41 @@ USE DemoCustomerNK_CN
 GO
 
 CREATE OR ALTER PROCEDURE [dbo].[YYY_sp_VTT_Customer_LoadList]
-    @Cust_Code NVARCHAR(50),
-    @limit INT
+    @PageNumber INT = 1,
+    @Limit INT = 10
 AS
 BEGIN
-    SELECT TOP (@limit) 
-		Cust_Code
-		,Name
-		,Phone1
-		,Note
-    FROM VTT_Customer
-    WHERE State = 1 
-      AND (@Cust_Code = '' OR Cust_Code = @Cust_Code)
-    ORDER BY Cust_Code DESC;
+    DECLARE @TotalRows INT = (SELECT COUNT(*) FROM VTT_Customer WHERE State = 1);
+
+	DECLARE @Offset INT = (@PageNumber - 1) * @Limit;
+
+	SELECT
+		  ID
+		, Name
+		, Cust_Code
+		, Phone1
+		, Note
+		, TotalRows = @TotalRows 
+	FROM VTT_Customer C 
+	WHERE State = 1
+	ORDER BY ID
+	OFFSET @Offset ROWS
+	FETCH NEXT @Limit ROW ONLY
 END
 GO
 
 CREATE OR ALTER PROCEDURE [dbo].[YYY_sp_VTT_Customer_LoadDetail]
-    @Cust_Code NVARCHAR(50)
+    @ID INT
 AS
 BEGIN
-    SELECT Cust_Code, Phone1, Name, Note 
+    SELECT ID, Cust_Code, Phone1, Name, Note 
     FROM VTT_Customer 
-    WHERE Cust_Code = @Cust_Code AND State = 1;
+    WHERE ID = @ID AND State = 1;
 END
 GO
 
 CREATE OR ALTER PROCEDURE [dbo].[YYY_sp_VTT_Customer_Insert]
-    @Cust_Code NVARCHAR(50)
+      @Cust_Code NVARCHAR(50)
     , @Name NVARCHAR(250)
     , @Note NVARCHAR(MAX)
 	, @Phone1 NVARCHAR(150)
@@ -43,7 +50,8 @@ END
 GO
 
 CREATE OR ALTER PROCEDURE [dbo].[YYY_sp_VTT_Customer_Update]
-    @Cust_Code NVARCHAR(50)
+	  @ID INT
+    , @Cust_Code NVARCHAR(50)
     , @Name NVARCHAR(250)
     , @Note NVARCHAR(MAX)
 	, @Phone1 NVARCHAR(150)
@@ -54,30 +62,30 @@ BEGIN
         , Name = @Name
         , Note = @Note
 		, Phone1 = @Phone1
-    WHERE Cust_Code = @Cust_Code;
+    WHERE ID = @ID;
 
     SELECT 1 AS RESULT, @Cust_Code AS Cust_Code;
 END
 GO
 
 CREATE OR ALTER PROCEDURE [dbo].[YYY_sp_VTT_Customer_Delete]
-    @Cust_Code NVARCHAR(50)
+    @ID INT
 AS
 BEGIN
     UPDATE VTT_Customer
     SET State = 0
-    WHERE Cust_Code = @Cust_Code;
+    WHERE ID = @ID;
     SELECT 1 AS RESULT;
 END
 GO
 
 
 EXEC [dbo].[YYY_sp_VTT_Customer_LoadList] 
-    @Cust_Code = N'', 
-    @limit = 10;
+    @PageNumber = 2, 
+    @Limit = 10;
 
 
-EXEC [dbo].[YYY_sp_VTT_Customer_LoadDetail] @Cust_Code = ''
+EXEC [dbo].[YYY_sp_VTT_Customer_LoadDetail] @ID = 1
 
-EXEC [dbo].[YYY_sp_VTT_Customer_Delete] @Cust_Code = ''
+EXEC [dbo].[YYY_sp_VTT_Customer_Delete] @ID = 1
   
