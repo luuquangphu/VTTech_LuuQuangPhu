112ea4828aa0bcc1908600724d41baf19ca6cad6
Lưu Quang Phú <luuquangphuii@gmail.com>
2026-05-12 17:07:08 +0700

    Thêm trường avatar vào SP LoadDetail để load hình ảnh

--- docs/StoreProcedure/sp_Customer.sql
+++ docs/StoreProcedure/sp_Customer.sql
@@ -1,4 +1,4 @@
-﻿USE DemoCustomerNK_CN
+USE DemoCustomerNK_CN
 GO
 
 CREATE OR ALTER PROCEDURE [dbo].[YYY_sp_VTT_Customer_LoadList]
@@ -38,60 +38,60 @@ CREATE OR ALTER PROCEDURE [dbo].[YYY_sp_VTT_Customer_LoadDetail]
     @ID INT
 AS
 BEGIN
-    SELECT ID, Cust_Code, Phone1, Name, Note 
+    SELECT ID, Cust_Code, Phone1, Name, Note, Avatar
     FROM VTT_Customer 
     WHERE ID = @ID AND State = 1;
 END
 GO
 
-CREATE OR ALTER PROCEDURE [dbo].[YYY_sp_VTT_Customer_Insert]
-      @Cust_Code NVARCHAR(50)
-    , @Name NVARCHAR(250)
-    , @Note NVARCHAR(MAX)
-	, @Phone1 NVARCHAR(150)
-AS
-BEGIN
-    INSERT INTO VTT_Customer (Cust_Code, Name, Note, Phone1, Created, Created_By, State)
-    VALUES (@Cust_Code, @Name, @Note, @Phone1, GETDATE(), 1, 1);
+	CREATE OR ALTER PROCEDURE [dbo].[YYY_sp_VTT_Customer_Insert]
+		  @Cust_Code NVARCHAR(50)
+		, @Name NVARCHAR(250)
+		, @Note NVARCHAR(MAX)
+		, @Phone1 NVARCHAR(150)
+	AS
+	BEGIN
+		INSERT INTO VTT_Customer (Cust_Code, Name, Note, Phone1, Created, Created_By, State)
+		VALUES (@Cust_Code, @Name, @Note, @Phone1, GETDATE(), 1, 1);
     
-    DECLARE @NewID INT = SCOPE_IDENTITY();
+		DECLARE @NewID INT = SCOPE_IDENTITY();
 
-    SELECT 
-		ID
-		, Cust_Code
-		, Name
-		, Phone1
-		, Note
-    FROM VTT_Customer
-    WHERE ID = @NewID;
-END
-GO
+		SELECT 
+			ID
+			, Cust_Code
+			, Name
+			, Phone1
+			, Note
+		FROM VTT_Customer
+		WHERE ID = @NewID;
+	END
+	GO
 
-CREATE OR ALTER PROCEDURE [dbo].[YYY_sp_VTT_Customer_Update]
-	  @ID INT
-    , @Cust_Code NVARCHAR(50)
-    , @Name NVARCHAR(250)
-    , @Note NVARCHAR(MAX)
-	, @Phone1 NVARCHAR(150)
-AS
-BEGIN
-    UPDATE VTT_Customer
-    SET Cust_Code = @Cust_Code
-        , Name = @Name
-        , Note = @Note
-		, Phone1 = @Phone1
-    WHERE ID = @ID;
+	CREATE OR ALTER PROCEDURE [dbo].[YYY_sp_VTT_Customer_Update]
+		  @ID INT
+		, @Cust_Code NVARCHAR(50)
+		, @Name NVARCHAR(250)
+		, @Note NVARCHAR(MAX)
+		, @Phone1 NVARCHAR(150)
+	AS
+	BEGIN
+		UPDATE VTT_Customer
+		SET Cust_Code = @Cust_Code
+			, Name = @Name
+			, Note = @Note
+			, Phone1 = @Phone1
+		WHERE ID = @ID;
 
-    SELECT	
-		ID
-		, Name
-		, Cust_Code
-		, Phone1
-		, Note
-	FROM VTT_Customer
-	WHERE ID = @ID
-END
-GO
+		SELECT	
+			ID
+			, Name
+			, Cust_Code
+			, Phone1
+			, Note
+		FROM VTT_Customer
+		WHERE ID = @ID
+	END
+	GO
 
 CREATE OR ALTER PROCEDURE [dbo].[YYY_sp_VTT_Customer_Delete]
     @ID INT
